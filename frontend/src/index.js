2cd77fa9196551a35077c400360988fbd6ef936a
loswithaurea <neil.gamboa.chester@gmail.com>
2022-11-29 05:15:00 +0800

    try

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,8 @@ import App from './App';
 import "bulma/css/bulma.css";
 import axios from "axios";
 import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
 
 ReactDOM.render(
