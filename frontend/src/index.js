c52b075ddb91084448cc492b4c7b6674ef747af1
omniiiiii <127201273+omniiiiii@users.noreply.github.com>
2023-04-22 14:23:08 +0800

    fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import "bulma/css/bulma.css";
 import axios from "axios";
 import { Amplify } from 'aws-amplify';
 
